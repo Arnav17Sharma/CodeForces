22c331a3e84cfaa2aeb7a765ae93e883bb320754
Arnav17Sharma <arnavsharma1702@gmail.com>
2023-12-28 01:23:33 +0530

    1433A - Boring Apartments

---
+++ A_Boring_Apartments.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define f(i,j) for(int i=0;i<j;i++)
+#define rf(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void answertoProb()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int k, g=0;
+        cin >> k;
+        int l = k%10;
+        int s = (l-1)*10;
+        while(k!=0){
+            k/=10;
+            g++;
+            s += g;
+        }
+        cout << s << endl;
+    }
+}
+
+
+int32_t main()
+{
+fastio()
+answertoProb();
+return 0;
+}
