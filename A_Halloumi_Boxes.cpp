92b7fa5c88aa88d34f40d226f3dd91103904f984
Arnav17Sharma <arnavsharma1702@gmail.com>
2024-04-07 00:10:26 +0530

    overall

--- A_Halloumi_Boxes.cpp
+++ A_Halloumi_Boxes.cpp
@@ -19,29 +19,16 @@ void answertoProb()
 {
     int t;
     cin >> t;
-    while(t--){
-        int n, k, l;
-        bool ans = true;
+    while(t--) {
+        long long n, k, s = 0;
         cin >> n >> k;
-        vi l1(n, 0);
-        vi l2(n, 0);
-        for(int i = 0; i<n; i++){
-            cin >> l;
-            l1[i] = l;
-            l2[i] = l;
+        vector<int> l(n);
+        for(int i=0; i<n; i++) {
+            cin >> l[i];
+            s += l[i];
         }
-        sort(all(l1));
-        int eq = 1;
-        for(int i=0; i<n; i++){
-            if(l1[i]!=l2[i]){eq = 0;}
-        }
-        if(eq){cout << "YES" << endl;}
-        else{
-        f(i,n){
-            int diff = abs((find(all(l1), l2[i]) - l1.begin()) - i) + 1;
-            if(diff > k && ans){cout << "NO" << endl;ans=false;}
-        }
-        if(ans){cout << "YES" << endl;}}
+        if(k==1 && !is_sorted(l.begin(), l.end())) cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
 }
 
